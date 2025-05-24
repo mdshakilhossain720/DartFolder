d6e69ed0f513ada30bc217c4e04b7ba723f14cb0
MD shakil Hossain <mdshakilhossain720@gmail.com>
2025-05-24 22:27:13 +0600

    dart

---
+++ bin/dartfloder.dart
@@ -0,0 +1,5 @@
+import 'package:dartfloder/dartfloder.dart' as dartfloder;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dartfloder.calculate()}!');
+}
---
+++ test/dartfloder_test.dart
@@ -0,0 +1,8 @@
+import 'package:dartfloder/dartfloder.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
